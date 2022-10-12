46ec1d813915af65b7adce23647f0dc62cee5937
Nadim <hussennadim49@gmail.com>
2022-10-12 19:21:48 +0600

    just a trick to impress your crush

--- autogui.py
+++ autogui.py
@@ -3,6 +3,6 @@ import time
 time.sleep(4)
 count=0
 while count<=350:
-    pyautogui.typewrite("hello bondhu sob bala nis"+str(count))
+    pyautogui.typewrite("I Love You "+str(count))
     pyautogui.press('enter')
     count = count+1
